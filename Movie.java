791a1d06d6220b039ac12f862819fd5b3b4ff9d0
Vitor Rezende <53321198+vterez@users.noreply.github.com>
2022-02-04 21:28:09 +0000

    Commit 14

--- Movie.java
+++ Movie.java
@@ -41,11 +41,6 @@ public class Movie {
   }
   public int getFrequentRenterPoints(int daysRented)
   {
-    // add frequent renter points
-        int frequentRenterPoints = 1;
-        // add bonus for a two day new release rental
-        if ((getPriceCode() == Movie.NEW_RELEASE) &&
-            daysRented > 1) frequentRenterPoints ++;
-        return frequentRenterPoints;
+    return _price.getFrequentRenterPoints(daysRented);
   }
 }
--- Price.java
+++ Price.java
@@ -1,4 +1,13 @@
 public abstract class Price {
    public abstract int getPriceCode();
    public abstract double getCharge(int daysRented);
+   public int getFrequentRenterPoints(int daysRented)
+   {
+     // add frequent renter points
+        int frequentRenterPoints = 1;
+        // add bonus for a two day new release rental
+        if ((getPriceCode() == Movie.NEW_RELEASE) &&
+            daysRented > 1) frequentRenterPoints ++;
+        return frequentRenterPoints;
+   }
 }
